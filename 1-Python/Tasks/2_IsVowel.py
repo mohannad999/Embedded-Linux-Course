d9c161a4d387d02b89a4dfcef0afbaab8595242e
Mohannad <mohandzaatreh@gmail.com>
2023-08-11 16:24:47 +0300

    solve Second Task

---
+++ 1-Python/Tasks/2_IsVowel.py
@@ -0,0 +1,10 @@
+
+VowelCharacters={'e','o','u','i','a'}
+
+inputchar=input('pls Enter character :')
+
+if inputchar in VowelCharacters :
+    print(f'{inputchar} is a Vowel')
+else:
+    print(f'{inputchar} is not a Vowel') 
+        
