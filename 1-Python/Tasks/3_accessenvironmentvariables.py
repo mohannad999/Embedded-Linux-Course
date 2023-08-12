ad2987a1a293253ce3727f5b34a71c2e3ec470a4
Mohannad <mohandzaatreh@gmail.com>
2023-08-12 09:24:32 +0300

    changed files locations

--- 1-Python/Tasks/3_accessenvironmentvariables.py
+++
@@ -1,6 +0,0 @@
-import os
-for data in os.environ:
-   print(data)
-   print('-'*15)
-   print(os.environ[data])
-   print('='*30)
--- 1-Python/Tasks/5_PrintCalender.py
+++
@@ -1,6 +0,0 @@
-#write a python program to print the calendar of a given month and year
-
-import calendar
-y = int(input("Input the year : "))
-m = int(input("Input the month : "))
-print(calendar.month(y, m))
